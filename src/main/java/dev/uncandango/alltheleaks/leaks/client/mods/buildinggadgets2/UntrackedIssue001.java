bccd5a28f2e689c5ff98ee6c16c9373c2141e05a
Pietro Lopes <unknownrj@gmail.com>
2025-01-07 01:15:19 -0300

    fix: prevent render to disappear on bg2 after dimension travel

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/buildinggadgets2/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/buildinggadgets2/UntrackedIssue001.java
@@ -5,6 +5,7 @@ import com.direwolf20.buildinggadgets2.util.FakeRenderingWorld;
 import dev.uncandango.alltheleaks.annotation.Issue;
 import dev.uncandango.alltheleaks.mixin.UpdateableLevel;
 import dev.uncandango.alltheleaks.utils.ReflectionHelper;
+import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.common.NeoForge;
 
 import java.lang.invoke.VarHandle;
@@ -12,9 +13,11 @@ import java.lang.invoke.VarHandle;
 @Issue(modId = "buildinggadgets2", versionRange = "[1.3.7,)")
 public class UntrackedIssue001 {
 	public static final VarHandle FAKE_RENDERING_WORLD;
+	public static final VarHandle REAL_WORLD;
 
 	static {
 		FAKE_RENDERING_WORLD = ReflectionHelper.getFieldFromClass(VBORenderer.class, "fakeRenderingWorld", FakeRenderingWorld.class, true);
+		REAL_WORLD = ReflectionHelper.getFieldFromClass(FakeRenderingWorld.class, "realWorld", Level.class, false);
 	}
 
 	public UntrackedIssue001() {
@@ -23,6 +26,13 @@ public class UntrackedIssue001 {
 	}
 
 	private void clearFakeWorld(UpdateableLevel.RenderEnginesUpdated event) {
-		FAKE_RENDERING_WORLD.set((Object) null);
+		if (event.getLevel() == null) {
+			FAKE_RENDERING_WORLD.set((Object) null);
+		} else {
+			var world = FAKE_RENDERING_WORLD.get();
+			if (world != null) {
+				REAL_WORLD.set(world, event.getLevel());
+			}
+		}
 	}
 }
