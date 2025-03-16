9a0689615e84fe9a31bf8493800e73d12c7f87f8
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:46:29 -0300

    feat: update geckolib

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/geckolib/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/geckolib/UntrackedIssue001.java
@@ -2,7 +2,7 @@ package dev.uncandango.alltheleaks.leaks.client.mods.geckolib;
 
 import dev.uncandango.alltheleaks.annotation.Issue;
 
-@Issue(modId = "geckolib", versionRange = "[4.4.8,)", mixins = "main.GeoModelMixin",
-description = "Uses WeakReferences to prevent memory leaks on `GeoModel#applyMolangQueries()`")
+@Issue(modId = "geckolib", versionRange = "[4.4.8,)", mixins = {"main.GeoModelMixin","main.GeoArmorRendererMixin"},
+description = "Uses WeakReferences to prevent memory leaks on `GeoModel#applyMolangQueries()` and updates entity at `GeoArmorRenderer#currentEntity`")
 public class UntrackedIssue001 {
 }
