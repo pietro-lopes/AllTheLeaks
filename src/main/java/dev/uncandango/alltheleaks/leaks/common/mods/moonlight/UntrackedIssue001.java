e69066b4207042e5176c16363f6b3ae8d4490afe
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:44:33 -0300

    feat: add moonlight

---
+++ src/main/java/dev/uncandango/alltheleaks/leaks/common/mods/moonlight/UntrackedIssue001.java
@@ -0,0 +1,7 @@
+package dev.uncandango.alltheleaks.leaks.common.mods.moonlight;
+
+import dev.uncandango.alltheleaks.annotation.Issue;
+
+@Issue(modId = "moonlight", versionRange = "[1.20-2.13.32,)", mixins = "main.FakeLevelManagerMixin", description = "Properly returns the levels being cleared")
+public class UntrackedIssue001 {
+}
