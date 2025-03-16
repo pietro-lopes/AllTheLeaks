6203505c15af27980e47f2d23210dd62831b4f70
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:53:16 -0300

    fix: update moonlight

---
+++ src/main/java/dev/uncandango/alltheleaks/mixin/core/main/FakeLevelManagerMixin.java
@@ -0,0 +1,20 @@
+package dev.uncandango.alltheleaks.mixin.core.main;
+
+import com.google.common.collect.Lists;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.mehvahdjukaar.moonlight.core.misc.FakeLevelManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Mixin(value = FakeLevelManager.class, remap = false)
+public class FakeLevelManagerMixin {
+	@WrapOperation(method = "invalidateAll", at = @At(value = "INVOKE", target = "Ljava/util/Map;values()Ljava/util/Collection;"))
+	private static Collection<Object> toImmutableMap(Map instance, Operation<Collection<Object>> original){
+		var map = original.call(instance);
+		return Lists.newArrayList(map);
+	}
+}
