1620feb4857cea372e2b90b5bf31fe6ed2f1cfb6
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:36:54 -0300

    fix: update travelersbackpack

--- src/main/java/dev/uncandango/alltheleaks/leaks/common/mods/travelersbackpack/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/common/mods/travelersbackpack/UntrackedIssue001.java
@@ -2,6 +2,6 @@ package dev.uncandango.alltheleaks.leaks.common.mods.travelersbackpack;
 
 import dev.uncandango.alltheleaks.annotation.Issue;
 
-@Issue(modId = "travelersbackpack", versionRange = "[9.1.0,)", mixins = "main.ForgeEventHandlerMixin", description = "Use reviveCaps instead of revive and adds invalidateCaps at `ForgeEventHandler#playerClone`")
+@Issue(modId = "travelersbackpack", versionRange = "[9.1.0,9.1.16]", mixins = "main.ForgeEventHandlerMixin", description = "Use reviveCaps instead of revive and adds invalidateCaps at `ForgeEventHandler#playerClone`")
 public class UntrackedIssue001 {
 }
---
+++ src/main/java/dev/uncandango/alltheleaks/leaks/common/mods/travelersbackpack/UntrackedIssue002.java
@@ -0,0 +1,7 @@
+package dev.uncandango.alltheleaks.leaks.common.mods.travelersbackpack;
+
+import dev.uncandango.alltheleaks.annotation.Issue;
+
+@Issue(modId = "travelersbackpack", versionRange = "[9.1.17,)", mixins = "main.NeoForgeEventHandlerMixin", description = "Use reviveCaps instead of revive and adds invalidateCaps at `NeoForgeEventHandler#playerClone`")
+public class UntrackedIssue002 {
+}
