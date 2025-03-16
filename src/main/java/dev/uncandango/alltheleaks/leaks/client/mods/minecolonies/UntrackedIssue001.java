8c4ab3a58c641c35e61449abfc43177460efcf06
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:44:04 -0300

    feat: update minecolonies

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/minecolonies/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/minecolonies/UntrackedIssue001.java
@@ -2,7 +2,7 @@ package dev.uncandango.alltheleaks.leaks.client.mods.minecolonies;
 
 import dev.uncandango.alltheleaks.annotation.Issue;
 
-@Issue(modId = "minecolonies", versionRange = "[1.20.1-1.1.647-beta,)", mixins = {"main.JobBasedRecipeCategoryMixin","main.JobBasedRecipeCategoryMixin$JobBasedRecipeCategoryAccessor", "main.GenericRecipeMixin"},
-	description = "Recreates citizens and entities on level update")
+@Issue(modId = "minecolonies", versionRange = "[1.20.1-1.1.647-beta,1.20.1-1.1.778-beta]", mixins = {"main.JobBasedRecipeCategoryMixin","main.JobBasedRecipeCategoryMixin$JobBasedRecipeCategoryAccessor"},
+	description = "Recreates citizens and entities on level update at `JobBasedRecipeCategory`")
 public class UntrackedIssue001 {
 }
---
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/minecolonies/UntrackedIssue002.java
@@ -0,0 +1,8 @@
+package dev.uncandango.alltheleaks.leaks.client.mods.minecolonies;
+
+import dev.uncandango.alltheleaks.annotation.Issue;
+
+@Issue(modId = "minecolonies", versionRange = "[1.20.1-1.1.647-beta,1.20.1-1.1.751-beta]", mixins = {"main.GenericRecipeMixin"},
+	description = "Recreates citizens and entities on level update at `GenericRecipe`")
+public class UntrackedIssue002 {
+}
