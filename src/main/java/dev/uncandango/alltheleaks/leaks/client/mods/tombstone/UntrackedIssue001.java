96c01c52b06c16b51bf30b17a2cbfc3693a59628
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:54:26 -0300

    fix: update tombstone

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/tombstone/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/tombstone/UntrackedIssue001.java
@@ -9,20 +9,23 @@ import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.invoke.VarHandle;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-@Issue(modId = "tombstone", versionRange = "[8.5.0,8.8.6]", mixins = "main.LivingEntityRendererMixin",
+@Issue(modId = "tombstone", versionRange = "[8.5.0,8.8.6]", mixins = "main.ATLLivingEntityRendererMixin",
 description = "Clears `LivingEntityRenderer#entity` added via mixin on client level update")
 public class UntrackedIssue001 {
 	public static final VarHandle LIVING_ENTITY;
 
 	static {
 		LIVING_ENTITY = ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "entity", LivingEntity.class, false);
+		var enabled = ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "atl$tombstoneEnabled", boolean.class, true);
+		enabled.set(true);
 	}
 
 	public static void clearLivingEntityFromRenderer(LivingEntityRenderer<?,?> renderer){
-		@Nullable var livingEntity = (LivingEntity) LIVING_ENTITY.get(renderer);
-		if (livingEntity instanceof LocalPlayer player || livingEntity != null && livingEntity.level() != Minecraft.getInstance().level) {
-			LIVING_ENTITY.set(renderer, (Object) null);
-		}
+			@Nullable var livingEntity = (LivingEntity) LIVING_ENTITY.get(renderer);
+			if (livingEntity instanceof LocalPlayer player || livingEntity != null && livingEntity.level() != Minecraft.getInstance().level) {
+				LIVING_ENTITY.set(renderer, (Object) null);
+			}
 	}
 }
