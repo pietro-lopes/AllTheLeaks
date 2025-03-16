2f0f5488946579d66d3c6f2adc26b768b6a1e491
Pietro Lopes <unknownrj@gmail.com>
2025-03-16 10:53:50 -0300

    fix: update emi

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/emi/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/emi/UntrackedIssue001.java
@@ -5,11 +5,12 @@ import dev.uncandango.alltheleaks.annotation.Issue;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-@Issue(modId = "emi", versionRange = "[1.0.21,)", description = "Clear `EmiHistory` when client player cloned")
+@Issue(modId = "emi", versionRange = "[1.0.21,)", description = "Clears `EmiHistory` when client player cloned or logout")
 public class UntrackedIssue001 {
 	public UntrackedIssue001() {
 		var gameBus = MinecraftForge.EVENT_BUS;
 		gameBus.addListener(this::clearPlayerFromHistory);
+		gameBus.addListener(this::clearPlayerFromHistoryOnLogout);
 	}
 
 	static {
@@ -21,6 +22,10 @@ public class UntrackedIssue001 {
 	}
 
 	private void clearPlayerFromHistory(ClientPlayerNetworkEvent.Clone event) {
+		EmiHistory.clear();
+	}
+
+	private void clearPlayerFromHistoryOnLogout(ClientPlayerNetworkEvent.LoggingOut event) {
 		EmiHistory.clear();
 	}
 }
