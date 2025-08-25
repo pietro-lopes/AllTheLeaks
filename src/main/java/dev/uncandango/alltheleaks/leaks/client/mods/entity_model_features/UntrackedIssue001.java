e93adf0f2933d07375f5286cae1142efa00234e0
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2025-08-25 00:03:24 -0300

    fix: ETF and EMF crashing on newest version

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/entity_model_features/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/entity_model_features/UntrackedIssue001.java
@@ -5,15 +5,23 @@ import dev.uncandango.alltheleaks.utils.ReflectionHelper;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.level.Level;
 import traben.entity_model_features.models.animation.EMFAnimationEntityContext;
+import traben.entity_model_features.utils.EMFEntity;
+import traben.entity_texture_features.utils.ETFEntity;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
 import java.lang.invoke.VarHandle;
 
-@Issue(modId = "entity_model_features", versionRange = "[2.2.2,)", mixins = "main.ATLLivingEntityRendererMixin", description = "Clears `LivingEntityRenderer#emf$heldIteration` when level changes")
+@Issue(modId = "entity_model_features", versionRange = "[2.2.2,3.0.0)", mixins = "main.ATLLivingEntityRendererMixin", description = "Clears `LivingEntityRenderer#emf$heldIteration` when level changes")
 public class UntrackedIssue001 {
 	public static final VarHandle EMF$HELDITERATION;
+	public static final MethodHandle EMF$IEMF_ENTITY;
+	public static final MethodHandle ETF$GET_WORLD;
 
 	static {
 		EMF$HELDITERATION = ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "emf$heldIteration", EMFAnimationEntityContext.IterationContext.class, false);
+		EMF$IEMF_ENTITY = ReflectionHelper.getMethodFromClass(EMFAnimationEntityContext.IterationContext.class, "IEMFEntity", MethodType.methodType(EMFEntity.class), false);
+		ETF$GET_WORLD = ReflectionHelper.getMethodFromClass(ETFEntity.class, "etf$getWorld", MethodType.methodType(Level.class), false);
 		var enabled = ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "atl$emfEnabled", boolean.class, true);
 		enabled.set(true);
 	}
--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/entity_texture_features/UntrackedIssue001.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/entity_texture_features/UntrackedIssue001.java
@@ -4,6 +4,7 @@ import dev.uncandango.alltheleaks.annotation.Issue;
 import dev.uncandango.alltheleaks.utils.ReflectionHelper;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
 import org.jetbrains.annotations.Nullable;
@@ -11,14 +12,23 @@ import traben.entity_texture_features.features.ETFManager;
 import traben.entity_texture_features.features.player.ETFPlayerEntity;
 import traben.entity_texture_features.utils.ETFEntity;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
 import java.lang.invoke.VarHandle;
+import java.util.Map;
 
-@Issue(modId = "entity_texture_features", versionRange = "[6.2.3,)", mixins = "main.ATLLivingEntityRendererMixin", description = "Clears `LivingEntityRenderer#etf$heldEntity` when level changes and updates player at `ETFManager#PLAYER_TEXTURE_MAP`")
+@Issue(modId = "entity_texture_features", versionRange = "[6.2.3,7.0.0)", mixins = "main.ATLLivingEntityRendererMixin", description = "Clears `LivingEntityRenderer#etf$heldEntity` when level changes and updates player at `ETFManager#PLAYER_TEXTURE_MAP`")
 public class UntrackedIssue001 {
 	public static final VarHandle ETF$HELDENTITY;
+	public static final MethodHandle ETF$GET_WORLD;
 
 	static {
 		ETF$HELDENTITY = ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "etf$heldEntity", ETFEntity.class, false);
+		if (!(ETFManager.getInstance().PLAYER_TEXTURE_MAP instanceof Map)) {
+			throw new IllegalArgumentException();
+		}
+		ReflectionHelper.getClass(ETFPlayerEntity.class.getName());
+		ETF$GET_WORLD = ReflectionHelper.getMethodFromClass(ETFEntity.class, "etf$getWorld", MethodType.methodType(Level.class), false);
 		var enabled = ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "atl$etfEnabled", boolean.class, true);
 		enabled.set(true);
 		var gameBus = MinecraftForge.EVENT_BUS;
