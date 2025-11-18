5f4157f5362ea6471601114f8695ae44b0a3e28e
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2025-11-18 14:24:10 -0300

    fix: prevent crash with alpha Iron's Spells
    Closes #58

--- src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/irons_spellbooks/UntrackedIssue002.java
+++ src/main/java/dev/uncandango/alltheleaks/leaks/client/mods/irons_spellbooks/UntrackedIssue002.java
@@ -2,8 +2,11 @@ package dev.uncandango.alltheleaks.leaks.client.mods.irons_spellbooks;
 
 import dev.uncandango.alltheleaks.annotation.Issue;
 import dev.uncandango.alltheleaks.mixin.UpdateableLevel;
+import dev.uncandango.alltheleaks.utils.ReflectionHelper;
 import io.redspace.ironsspellbooks.entity.mobs.dead_king_boss.DeadKingMusicManager;
 import net.minecraftforge.common.MinecraftForge;
+
+import java.lang.invoke.MethodType;
 
 @Issue(modId = "irons_spellbooks", versionRange = "[1.20.1-3.4.0,)",
 	description = "Clears `DeadKingMusicManager#INSTANCE` on client level update")
@@ -11,6 +14,10 @@ public class UntrackedIssue002 {
 	public UntrackedIssue002() {
 		var gamebus = MinecraftForge.EVENT_BUS;
 		gamebus.addListener(this::clearDeadKingMusicManager);
+	}
+
+	static {
+		var dummy = ReflectionHelper.getMethodFromClass(DeadKingMusicManager.class, "hardStop", MethodType.methodType(void.class), true);
 	}
 
 	private void clearDeadKingMusicManager(UpdateableLevel.RenderEnginesUpdated event) {
